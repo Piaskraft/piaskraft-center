20906098372bfb615d997dc465b5f2b82d7af55f
Piaskraft <piasecki.mateusz@interia.pl>
2026-07-18 16:55:57 +0200

    Connect NestJS to PostgreSQL with Prisma

---
+++ apps/api/src/prisma/prisma.service.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+import {
+  Injectable,
+  type OnModuleDestroy,
+  type OnModuleInit,
+} from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('Brak zmiennej DATABASE_URL.');
+    }
+
+    const adapter = new PrismaPg({
+      connectionString,
+    });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
